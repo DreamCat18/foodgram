c14ae1c401be7b26871b05aa1fbff9c54435aa50
Amelia <ameliahenituse@yandex.ru>
2025-10-15 15:48:28 +0300

    удалёнка

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 
-# def home(request):
-#     return JsonResponse({"message": "Welcome to Foodgram API"})
+def home(request):
+    return redirect('/api/')  
 
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
