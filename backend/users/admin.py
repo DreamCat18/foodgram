605655528460af17f48e73d640f70023fde5a5ef
Amelia <ameliahenituse@yandex.ru>
2025-11-10 22:06:38 +0300

    админка в юзере

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff')
+class CustomUserAdmin(UserAdmin):  
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff') 
     search_fields = ('email', 'username', 'first_name', 'last_name')
-    list_filter = ('is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'date_joined') 
     ordering = ('email',)
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
