059b8edc0feaf44f1a41d24df7f466303458de89
Amelia <ameliahenituse@yandex.ru>
2025-11-11 19:54:40 +0300

    пермишн

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class IsAuthorOrReadOnlyPermission(BasePermission):
     """Разрешение, позволяющее редактировать объект только автору."""
 
     def has_object_permission(self, request, view, obj):
