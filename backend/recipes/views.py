3af3ba9b5c9ce314a3951ea73b3a17d52b860ce1
Amelia <ameliahenituse@yandex.ru>
2025-11-15 12:36:26 +0300

    короткая ссылка

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,7 @@
 import base64
 
 from django.conf import settings
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 
 from .models import Recipe
 
@@ -11,9 +11,9 @@ def short_url(request, short_link):
     try:
         short_link += '=' * (4 - len(short_link) % 4)
         recipe_pk = int(base64.urlsafe_b64decode(short_link).decode())
-        recipe = get_object_or_404(Recipe, pk=recipe_pk)
+        recipe = Recipe.objects.get(pk=recipe_pk)
         return redirect(
             f"{settings.FRONTEND_BASE_URL}/recipes/{recipe.slug}"
         )
-    except (ValueError, UnicodeDecodeError):
-        return redirect('/api/')
+    except (ValueError, UnicodeDecodeError, Recipe.DoesNotExist):
+        return redirect('/not_found/')
