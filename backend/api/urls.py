01cd24f87d039b0e7f024dcdccb3363d6d7a7e78
Amelia <ameliahenituse@yandex.ru>
2025-11-18 19:28:05 +0300

    урлы

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,18 @@
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
+from api.views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
 from django.urls import include, path
-from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 
-router_v1.register('users', DjoserUserViewSet, basename='users')
+router_v1.register('users', UserViewSet, basename='users')
 router_v1.register('recipes', RecipeViewSet, basename='recipe')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredient')
 router_v1.register('tags', TagViewSet, basename='tag')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
+    path('auth/', include('djoser.urls')),  # без него не работает смена пароля
     path('auth/', include('djoser.urls.authtoken')),
 ]
